612eef5afe143ef94bbbd15831b86ec089e3c1ac
Nick Clason <nick.clason1@gmail.com>
2021-03-18 14:24:04 -0400

    Add more runtime functions

--- README.md
+++ README.md
@@ -9,5 +9,12 @@ make all
 ## How to run
 ```
 ./output/compiler
+
 clang output/IR.ll src/runtime.c
+            or
+gcc output/output.o src/runtime.c 
+
+Then run ./a.out
 ```
+
+
--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -116,33 +116,108 @@ std::map<std::string, Symbol> SymbolTable::GetLocalScope() {
 
 void SymbolTable::AddIOFunctions(llvm::Module *llvmModule, llvm::LLVMContext &llvmContext, llvm::IRBuilder<> *llvmBuilder)
 {
-    llvm::FunctionType *function_type = nullptr;
+    llvm::FunctionType *type = nullptr;
     llvm::Function *procedure = nullptr;
 
-    // putInteger(integer value) : bool
-    Symbol put_integer;
-    put_integer.SetId("PUTINTEGER");
-    put_integer.SetType(T_BOOL);
-    Symbol put_integer_arg;
-    put_integer_arg.SetId("value");
-    put_integer_arg.SetType(T_INTEGER);
-    put_integer_arg.SetDeclarationType(T_VARIABLE);
-    put_integer.GetParameters().push_back(put_integer_arg);
-    put_integer.SetIsGlobal(true);
-    put_integer.SetDeclarationType(T_PROCEDURE);
+    Symbol putInteger;
+    putInteger.SetId("PUTINTEGER");
+    putInteger.SetType(T_BOOL);
+    Symbol putIntegerArgs;
+    putIntegerArgs.SetId("num");
+    putIntegerArgs.SetType(T_INTEGER);
+    putIntegerArgs.SetDeclarationType(T_VARIABLE);
+    putInteger.GetParameters().push_back(putIntegerArgs);
+    putInteger.SetIsGlobal(true);
+    putInteger.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getInt1Ty(),{llvmBuilder->getInt32Ty()},false);
+    procedure = llvm::Function::Create(type,llvm::Function::ExternalLinkage,putInteger.GetId(),llvmModule);
+    putInteger.SetLLVMFunction(procedure);
+    AddSymbol(putInteger);
 
-        function_type = llvm::FunctionType::get(
-                llvmBuilder->getInt1Ty(),
-                {llvmBuilder->getInt32Ty()},
-                false);
-        procedure = llvm::Function::Create(
-                function_type,
-                llvm::Function::ExternalLinkage,
-                put_integer.GetId(),
-                llvmModule);
-        put_integer.SetLLVMFunction(procedure);
+    Symbol getInteger;
+    getInteger.SetId("GETINTEGER");
+    getInteger.SetType(T_INTEGER);
+    getInteger.SetIsGlobal(true);
+    getInteger.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getInt32Ty(), {},false);
+    procedure = llvm::Function::Create(type, llvm::Function::ExternalLinkage, getInteger.GetId(), llvmModule);
+    getInteger.SetLLVMFunction(procedure);
+    AddSymbol(getInteger);
 
-    AddSymbol(put_integer);
+    Symbol putFloat;
+    putFloat.SetId("PUTFLOAT");
+    putFloat.SetType(T_BOOL);
+    Symbol putFloatArgs;
+    putFloatArgs.SetId("num");
+    putFloatArgs.SetType(T_FLOAT);
+    putFloatArgs.SetDeclarationType(T_VARIABLE);
+    putFloat.GetParameters().push_back(putFloatArgs);
+    putFloat.SetIsGlobal(true);
+    putFloat.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getInt1Ty(),{llvmBuilder->getFloatTy()},false);
+    procedure = llvm::Function::Create(type,llvm::Function::ExternalLinkage,putFloat.GetId(),llvmModule);
+    putFloat.SetLLVMFunction(procedure);
+    AddSymbol(putFloat);
+
+    Symbol getFloat;
+    getFloat.SetId("GETFLOAT");
+    getFloat.SetType(T_FLOAT);
+    getFloat.SetIsGlobal(true);
+    getInteger.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getFloatTy(), {},false);
+    procedure = llvm::Function::Create(type, llvm::Function::ExternalLinkage, getFloat.GetId(), llvmModule);
+    getFloat.SetLLVMFunction(procedure);
+    AddSymbol(getFloat);
+
+    Symbol putBool;
+    putBool.SetId("PUTBOOL");
+    putBool.SetType(T_BOOL);
+    Symbol putBoolArgs;
+    putBoolArgs.SetId("num");
+    putBoolArgs.SetType(T_BOOL);
+    putBoolArgs.SetDeclarationType(T_VARIABLE);
+    putBool.GetParameters().push_back(putBoolArgs);
+    putBool.SetIsGlobal(true);
+    putBool.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getInt1Ty(),{llvmBuilder->getInt1Ty()},false);
+    procedure = llvm::Function::Create(type,llvm::Function::ExternalLinkage, putBool.GetId(), llvmModule);
+    putBool.SetLLVMFunction(procedure);
+    AddSymbol(putBool);
+
+    Symbol getBool;
+    getBool.SetId("GETBOOL");
+    getBool.SetType(T_BOOL);
+    getBool.SetIsGlobal(true);
+    getBool.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getInt1Ty(), {},false);
+    procedure = llvm::Function::Create(type, llvm::Function::ExternalLinkage, getBool.GetId(), llvmModule);
+    getBool.SetLLVMFunction(procedure);
+    AddSymbol(getBool);
+
+    Symbol putString;
+    putString.SetId("PUTSTRING");
+    putString.SetType(T_BOOL);
+    Symbol putStringArgs;
+    putStringArgs.SetId("str");
+    putStringArgs.SetType(T_STRING);
+    putStringArgs.SetDeclarationType(T_VARIABLE);
+    putString.GetParameters().push_back(putStringArgs);
+    putString.SetIsGlobal(true);
+    putString.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getInt1Ty(),{llvmBuilder->getInt8PtrTy()},false);
+    procedure = llvm::Function::Create(type,llvm::Function::ExternalLinkage,putString.GetId(),llvmModule);
+    putString.SetLLVMFunction(procedure);
+    AddSymbol(putString);
+
+    Symbol getString;
+    getString.SetId("GETSTRING");
+    getString.SetType(T_STRING);
+    getString.SetIsGlobal(true);
+    getString.SetDeclarationType(T_PROCEDURE);
+    type = llvm::FunctionType::get(llvmBuilder->getInt8Ty(), {},false);
+    procedure = llvm::Function::Create(type, llvm::Function::ExternalLinkage, getString.GetId(), llvmModule);
+    getString.SetLLVMFunction(procedure);
+    AddSymbol(getString);
 
 }
 
