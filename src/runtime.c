959b9177f052470173d4afdb99ce4ceb638f3ab5
Nick Clason <nick.clason1@gmail.com>
2021-04-16 10:55:13 -0400

    Update runtime.c

--- src/runtime.c
+++ src/runtime.c
@@ -71,7 +71,8 @@ char* GETSTRING()
     char *string = malloc(max_length * sizeof(char));
     fgets(string, max_length, stdin);
     // remove trailing new line if there is one
-    if ((strlen(string) > 0) && (string[strlen(string) - 1] == '\n')) {
+    if ((strlen(string) > 0) && (string[strlen(string) - 1] == '\n'))
+    {
         string[strlen(string) - 1] = '\0';
     }
     return string;
